35d9069344ef77c4e00cda05417564c201cdf296
Adam Bogocz <adi_bog@o2.pl>
2023-04-02 20:06:14 +0200

    change format clock

--- src/App/Clock/clock.js
+++ src/App/Clock/clock.js
@@ -1,8 +1,10 @@
-import addDays from "date-fns/addDays";
-import { useEffect, useStage } from "react";
+import { format } from "date-fns";
+import { pl } from "date-fns/locale";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const NDate = () => {
-  const [date, setDate] = useStage(new Date());
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -13,7 +15,7 @@ const NDate = () => {
     };
   }, []);
 
-  return date;
+  return <p className="clock">{format(date, "do LLLL yyy - H:mm:ss", { locale: pl })}</p>
 };
 
 export default NDate;
