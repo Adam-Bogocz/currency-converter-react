8f207d3706439a5e5667d65cd9ec473eb360b1c8
Adam Bogocz <adi_bog@o2.pl>
2023-04-02 20:05:21 +0200

    change calculator sytle

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,7 @@ import NDate from "./Clock/clock";
 
 function App() {
   const [result, setResult] = useState();
-  
+
   const calculateResult = (direction, cash, currency, exchangeRate) => {
     if (direction === `PLN »`) {
       setResult({
@@ -28,17 +28,17 @@ function App() {
   };
 
   return (
-    <container>
-      <div className="app">
-        <section className="section__result">
-          <Result result={result} />
-        </section>
-        <section className="section__formRadio">
-          <NDate/>
-          <Form result={result} calculateResult={calculateResult} />
-        </section>
-      </div>
-    </container>
+    <div className="calculator">
+      <section className="section__result">
+        <Result result={result} />
+      </section>
+      <section className="section__clock">
+        <NDate />
+      </section>
+      <section className="section__form">
+        <Form result={result} calculateResult={calculateResult} />
+      </section>
+    </div>
   );
 }
 
