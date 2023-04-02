6f2afcddac3c3c2ce9e04aeb140d9b24d2cfe5d7
Adam Bogocz <adi_bog@o2.pl>
2023-04-02 20:08:40 +0200

    change style result

--- src/App/Result/result.js
+++ src/App/Result/result.js
@@ -3,13 +3,13 @@ import "./resultStyle.css";
 export const Result = ({ result }) => {
   
   return (
-    <>
+    <p className="result">
       {!!result && (
         <>
           {result.cash}&nbsp;{result.sourceCurrency} ={" "}
           {result.targetAmount.toFixed(2)}&nbsp;{result.targetCurrency}
         </>
       )}
-    </>
+    </p>
   );
 };
